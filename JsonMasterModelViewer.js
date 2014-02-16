55fbf8a51bebbcfdefbe254ba544c13d1f30c31f
itkr <g600392@gmail.com>
2014-02-16 22:56:45 +0900

    Update unit

--- JsonMasterModelViewer.js
+++ JsonMasterModelViewer.js
@@ -475,11 +475,13 @@
 		showColumns($('cb'), model);
 		showPulldown($('sk'), model.columns);
 
+		viewLength = $('length').value
 		pager = JMMV.getPager(model.getDatas(mkFilter()), viewLength);
 		showData($('container'), pager.getCurrent(), getCheckboxValues($(JMMV.ID.COLUMN_CHECKBOX)));
 		$('pager_count').innerHTML = "" + pager.getCurrentNumber() + "/" + pager.getMaxNumber();
 
 		JMMV.on($('show'), 'click', function() {
+			viewLength = $('length').value
 			pager = JMMV.getPager(model.getDatas(mkFilter()), viewLength);
 			showData($('container'), pager.getCurrent(), getCheckboxValues($(JMMV.ID.COLUMN_CHECKBOX)));
 			$('pager_count').innerHTML = "" + pager.getCurrentNumber() + "/" + pager.getMaxNumber();
